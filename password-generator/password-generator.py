ad284f51c4186a4a5350175f82ab1a04954b09b8
Israeli23 <148721062+Israeli23@users.noreply.github.com>
2024-02-05 18:26:19 +0000

    Update password-generator.py

--- password-generator/password-generator.py
+++ password-generator/password-generator.py
@@ -1,1 +1,40 @@
+import random 
+import string
+
+
+def password_generator(min_length, numbers=True, special_characters=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters += special
+    
+    password = " "
+    meets_criteria = False
+    has_number = False
+    has_special = False
+
+    while not meets_criteria or len(password) < min_length:
+        new_char = random.choice(characters)
+
+        if new_char in digits:
+            has_number = True
+        elif new_char in special:
+            has_special = True
+
+        meets_criteria = True
+        if numbers:
+            meets_criteria = has_number
+        if special:
+            meets_criteria = meets_criteria and has_special
+
+    return password
+
+password = password_generator(10)
+print(password)
+
 
